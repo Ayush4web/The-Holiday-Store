23114db1d8b26edc2bc93c40751cf6a6d48c6ea8
Ayush <choubeyayush4@gmail.com>
2022-03-25 14:36:39 +0530

    done

--- js/custom.js
+++ js/custom.js
@@ -2,15 +2,16 @@ const list = document.querySelectorAll('.item')
 const btnList = document.querySelectorAll('.list-group-item')
 
 const handle = (id) => {
- for (let i = 0; i < list.length; i++) {
-    list[i].classList.remove("active")
-   
- }
- for (let i = 0; i < btnList.length; i++) {
-    btnList[i].classList.remove("actv")
-   
- }
- 
- list[id-1].classList.add("active")
- btnList[id-1].classList.add("actv")
+  for (let i = 0; i < list.length; i++) {
+    list[i].classList.remove('active')
+  }
+  for (let i = 0; i < btnList.length; i++) {
+    btnList[i].classList.remove('actv')
+  }
+
+  list[id - 1].classList.add('active')
+  btnList[id - 1].classList.add('actv')
 }
+
+
+let a = document.getElementsByClassName("icon_phone").addEventListener()
